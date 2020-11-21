5aef168fd78137e55052c0313e57417e346b70df
xGodScent <71233972+xGodScent@users.noreply.github.com>
2020-11-21 19:03:43 +0100

    meta

--- pages/lib/scripts/ScrollView.js
+++ pages/lib/scripts/ScrollView.js
@@ -1,10 +1,9 @@
-$(document).ready(function(){
-  $(window).scroll(function() {
-      if ($(this).scrollTop() > 0) {// can be whatever, 0 refers to the top space you allow
-          $('.move').hide();// Hide your element
-      }
-      else {
-          $('.move').show();// It's just if you want to show back the element if we're back on top
-      }
-  });
-});
+window.addEventListener("scroll", function() {showFunction()});
+
+function showFunction() {
+    if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
+        document.getElementById("toptexts2").style.display = "block";
+    } else {
+        document.getElementById("toptexts2").style.display = "none";
+    }
+}
