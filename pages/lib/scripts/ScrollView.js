8c6d6dfcc30598a9e5eec76b55d0354d0cf10fa2
xGodScent <71233972+xGodScent@users.noreply.github.com>
2020-11-21 20:14:46 +0100

    update, doesnt work

--- pages/lib/scripts/ScrollView.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function() {showFunction()});
-
-function showFunction() {
-    if (document.body.scrollTop > 900 || document.documentElement.scrollTop > 900) {
-        document.getElementById("toptexts2").style.display = "block";
-    } else {
-        document.getElementById("toptexts2").style.display = "none";
-    }
-}
---
+++ pages/lib/scripts/show-on-scroll.js
@@ -0,0 +1,10 @@
+
+window.addEventListener('scroll', function() {
+  var rect = element.getBoundingClientRect();
+
+  var element = window.screen.height + window.scrollY
+  console.log(element);
+
+  console.log(rect);
+
+});
